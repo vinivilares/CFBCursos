207c924227d4d2bbe5232ea683c1ad90add1a302
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2021-05-26 17:12:25 -0300

    if else

---
+++ Aula 07/script.js
@@ -0,0 +1,14 @@
+var v1, v2, tx1, tx2;
+v1 = 10;
+v2 = 15;
+
+tx1 = "CFB Cursos";
+tx2 = "Curso de javascript";
+
+if(v1 == v2) {
+    document.write(tx1);
+    alert(tx1);
+} else {
+    document.write(tx2);
+    alert(tx2);
+}
