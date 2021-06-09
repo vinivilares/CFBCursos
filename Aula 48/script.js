7e2c4ded144a21a7980807cf4ca9c517897d21d7
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2021-06-09 20:37:55 -0300

    Métodos para manipulação de string P3

---
+++ Aula 48/script.js
@@ -0,0 +1,14 @@
+var texto = document.getElementById("txt").innerHTML;
+
+// var res = texto1.indexOf('j'); // retorna a primeira posição encontrada
+// var res = texto1.lastIndexOf('j'); // retorna a ultima posição encontrada
+// var res = texto1.localeCompare(texto2); // compara as strings
+// 0 = iguais
+// !=0 diferentes
+// var res = texto.slice(22, 33); // corta a string na posição indicada
+// var res = texto.split(" "); // corta a string nos caracteres
+// var res = texto.substr(4, 6); // define o tamanho do corte
+// var res = texto.toLocaleLowerCase(); //converte para minusculo
+// var res = texto.toUpperCase(); //converte para maiusculo
+
+document.write(res);
