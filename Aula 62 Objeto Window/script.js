422dba4f9175a2c68ddba6bd23fdd2e730d847f0
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2021-08-04 21:02:14 -0300

    Update

---
+++ Aula 62 Objeto Window/script.js
@@ -0,0 +1,11 @@
+let minhaJanela1, minhaJanela2;
+
+function abreJanela() {
+    minhaJanela1 = window.open("tmp.html", "_blank", "width =300, height=300, top=300, left=300");
+    minhaJanela2 = window.open("tmp.html", "_blank", "width =300, height=300, top=350, left=350");
+}
+
+function fechaJanela() {
+    minhaJanela1.close();
+    minhaJanela2.close();
+}
